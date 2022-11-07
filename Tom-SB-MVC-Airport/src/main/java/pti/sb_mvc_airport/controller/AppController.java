5ddc872d49291e3a228558a1a482d4bf09a2b5df
bogdanovTeam7 <bogdanov@t-email.hu>
2022-11-07 11:19:03 +0100

    first task

--- Tom-SB-MVC-Airport/src/main/java/pti/sb_mvc_airport/controller/AppController.java
+++ Tom-SB-MVC-Airport/src/main/java/pti/sb_mvc_airport/controller/AppController.java
@@ -1,8 +1,28 @@
 package pti.sb_mvc_airport.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import pti.sb_mvc_airport.db.Database;
+import pti.sb_mvc_airport.model.Flight;
 
 @Controller
 public class AppController {
+	@GetMapping("/")
+	public String home() {
+		return "home.html";
+	}
 
+	@GetMapping("/allFlights")
+	public String allFlights(Model model) {
+		Database db = new Database();
+		List<Flight> flights = db.getAllFlights();
+		db.close();
+		model.addAttribute("flights", flights);
+		
+		return "allFlights.html";
+	}
 }
--- Tom-SB-MVC-Airport/src/main/java/pti/sb_mvc_airport/db/Database.java
+++ Tom-SB-MVC-Airport/src/main/java/pti/sb_mvc_airport/db/Database.java
@@ -1,9 +1,17 @@
 package pti.sb_mvc_airport.db;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import pti.sb_mvc_airport.model.Flight;
 
 public class Database {
 	private SessionFactory factory;
@@ -15,5 +23,15 @@ public class Database {
 
 	public void close() {
 		factory.close();
+	}
+
+	public List<Flight> getAllFlights() {
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		Query query = session.createQuery("SELECT f FROM Flight f ORDER BY f.dateFrom", Flight.class);
+		List<Flight> flights = query.getResultList();
+		transaction.commit();
+		session.close();
+		return flights;
 	}
 }
