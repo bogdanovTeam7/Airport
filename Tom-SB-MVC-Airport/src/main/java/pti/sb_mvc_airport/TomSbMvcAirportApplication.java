5d2c8a80c961d2e301f32e42b2876ded2d03a6a5
bogdanovTeam7 <bogdanov@t-email.hu>
2022-11-07 10:41:47 +0100

    init

---
+++ Tom-SB-MVC-Airport/src/main/java/pti/sb_mvc_airport/TomSbMvcAirportApplication.java
@@ -0,0 +1,13 @@
+package pti.sb_mvc_airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomSbMvcAirportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomSbMvcAirportApplication.class, args);
+	}
+
+}
---
+++ Tom-SB-MVC-Airport/src/test/java/pti/sb_mvc_airport/TomSbMvcAirportApplicationTests.java
@@ -0,0 +1,13 @@
+package pti.sb_mvc_airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomSbMvcAirportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
